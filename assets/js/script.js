8cfc955efd5009877b2c451999ee6ee86c72444f
Alex <alexsala112@gmail.com>
2023-12-02 04:10:53 -0500

    Version 2 - Improved slideshow with categories

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,15 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+afiseazaSlide(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function afiseazaSlide(n) {
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  
+  // Ascunde toate slide-urile
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  // Arată slide-ul dorit
+  slides[n - 1].style.display = "block";
 }
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slide");
-    let slideTexts = document.getElementsByClassName("slide-text");
-
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"; // Ascunde toate slide-urile
-        slideTexts[i].style.display = "none"; // Ascunde toate div-urile cu descrieri
-    }
-
-    slides[slideIndex - 1].style.display = "block"; // Afișează slide-ul curent
-    slideTexts[slideIndex - 1].style.display = "block"; // Afișează div-ul cu descrierea corespunzătoare
-}
-
-document.querySelector(".prev").addEventListener("click", function() {
-    plusSlides(-1);
-});
-
-document.querySelector(".next").addEventListener("click", function() {
-    plusSlides(1);
-});
