ce2e319125dffdf58c583276ac6302ab421eddf7
Alex <alexsala112@gmail.com>
2023-12-03 09:33:28 -0500

    V5 - Improved code 1 function for small slideshows

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 //SLIDESHOW MARE
 let slideIndex0 = 1;
+let smallSlides;
+let slideIndex1 = 1;
 showSlides(slideIndex0);
 
 function showSlides(n) {
@@ -13,12 +15,19 @@ function showSlides(n) {
 
   // Arată slide-ul dorit 
   slides[n - 1].style.display = "block";
+
+  if (n===1) 
+    {smallSlides = document.querySelectorAll(".slide-mic-js-1")
+  } else if (n===2)
+    {smallSlides= document.querySelectorAll(".slide-mic-js-2")
+  };
+  showSlides1(1);
 }
 // END SLIDESHOW MARE
 
 //SLIDESHOW MIC 1
-let slideIndex1 = 1;
-showSlides1(slideIndex1);
+//let slideIndex1 = 1;
+//showSlides1(slideIndex1);
 
 // Next/previous controls
 function plusSlides1(n) {
@@ -27,15 +36,15 @@ function plusSlides1(n) {
 
 function showSlides1(n) {
   let i;
-  let slides = document.querySelectorAll(".slide-mic-js-1");
-  if (n > slides.length) {slideIndex1 = 1}
-  if (n < 1) {slideIndex1 = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  //let slides = document.querySelectorAll(".slide-mic-js-1");
+  if (n > smallSlides.length) {slideIndex1 = 1}
+  if (n < 1) {slideIndex1 = smallSlides.length}
+  for (i = 0; i < smallSlides.length; i++) {
+    smallSlides[i].style.display = "none";
   }
-  slides[slideIndex1-1].style.display = "block";
+  smallSlides[slideIndex1-1].style.display = "block";
 }
-
+/*
 //SLIDESHOW MIC 2
 let slideIndex2 = 1;
 showSlides2(slideIndex2);
@@ -55,6 +64,6 @@ function showSlides2(n) {
   }
   slides[slideIndex2-1].style.display = "block";
 }
-
+*/
 
 
