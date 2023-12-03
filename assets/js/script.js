ae9e4ad8d2736619e5ee5a4af3cdd9996a0fd0c2
Alex <alexsala112@gmail.com>
2023-12-02 20:57:38 -0500

    V4 - Long code slideshow in slideshow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-let slideIndex = 1;
-afiseazaSlide(slideIndex);
+//SLIDESHOW MARE
+let slideIndex0 = 1;
+showSlides(slideIndex0);
 
-
-function afiseazaSlide(n) {
+function showSlides(n) {
   let i;
-  const slides = document.querySelectorAll(".slide");
+  const slides = document.querySelectorAll(".slide-mare");
   
   // Ascunde toate slide-urile
   for (i = 0; i < slides.length; i++) {
@@ -14,56 +14,47 @@ function afiseazaSlide(n) {
   // Arată slide-ul dorit 
   slides[n - 1].style.display = "block";
 }
+// END SLIDESHOW MARE
 
-let slideNumber = 1;
-afiseazaSlideMic1(1);
+//SLIDESHOW MIC 1
+let slideIndex1 = 1;
+showSlides1(slideIndex1);
 
-function afiseazaSlideMic1(n) {
+// Next/previous controls
+function plusSlides1(n) {
+  showSlides1(slideIndex1 += n);
+}
+
+function showSlides1(n) {
   let i;
-  const slideMic = document.querySelectorAll(".slideshowMic1");
-
-  // Ascunde toate slide-urile mici
-  for (i = 0; i < slideMic.length; i++) {
-    slideMic[i].style.display = "none";
+  let slides = document.querySelectorAll(".slide-mic-js-1");
+  if (n > slides.length) {slideIndex1 = 1}
+  if (n < 1) {slideIndex1 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  slides[slideIndex1-1].style.display = "block";
+}
 
-  // Ajustează indexul slide-ului mic în funcție de direcție
-  slideNumber += n;
+//SLIDESHOW MIC 2
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
 
-  // Asigură-te că indexul se află în limitele array-ului de slide-uri mici
-  if (slideNumber > slideMic.length) {
-    slideNumber = 1;
-  } else if (slideNumber < 1) {
-    slideNumber = slideMic.length;
+// Next/previous controls
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+function showSlides2(n) {
+  let i;
+  let slides = document.querySelectorAll(".slide-mic-js-2");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-
-  // Arată slide-ul mic dorit
-  slideMic[slideNumber - 1].style.display = "block";
+  slides[slideIndex2-1].style.display = "block";
 }
 
 
-let slideNumber2 = 1;
-afiseazaSlideMic2(1);
 
-function afiseazaSlideMic2(n) {
-  let i;
-  const slideMic = document.querySelectorAll(".slideshowMic2");
-
-  // Ascunde toate slide-urile mici
-  for (i = 0; i < slideMic.length; i++) {
-    slideMic[i].style.display = "none";
-  }
-
-  // Ajustează indexul slide-ului mic în funcție de direcție
-  slideNumber2 += n;
-
-  // Asigură-te că indexul se află în limitele array-ului de slide-uri mici
-  if (slideNumber2 > slideMic.length) {
-    slideNumber2 = 1;
-  } else if (slideNumber2 < 1) {
-    slideNumber2 = slideMic.length;
-  }
-
-  // Arată slide-ul mic dorit
-  slideMic[slideNumber2 - 1].style.display = "block";
-}
